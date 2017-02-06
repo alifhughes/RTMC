8eb9bee7fca54db6673ac7f072fa26fcb648d2cd
alifhughes <alistair.hughes@students.plymouth.ac.uk>
2017-02-06 20:54:31 +0000

    basic frontpage completed- buttons present but not working

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Real-Time Music Collaborator' });
+  res.render(
+    'index',
+    {
+        title: 'Real-Time Music Collaborator',
+        buttonText1: 'Login',
+        buttonText2: 'Start Creating',
+    }
+  );
 });
 
 module.exports = router;
